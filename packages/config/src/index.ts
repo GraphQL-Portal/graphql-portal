d84c5401dc374a110a75b3961a9dd8e04af3a30a
Arthur Murauskas <arthur.murauskas@gmail.com>
2020-12-16 21:07:11 +0100

    Move config to a dedicated package

--- packages/config/src/index.ts
+++ packages/config/src/index.ts
@@ -1,5 +1,5 @@
 import { cosmiconfig } from 'cosmiconfig';
-import { prefixLogger } from '../../../src/logger';
+// import { prefixLogger } from '../../../src/logger';
 import { ApiDef } from './types/api.interface';
 import { join } from 'path';
 import { parse } from 'yaml';
@@ -8,6 +8,9 @@ import Ajv from 'ajv';
 import { GatewayConfig } from './types/gateway-config';
 import { ApiConfig } from './types/api-config';
 import { SourceConfig } from './types/mesh-source-config';
+import * as gatewaySchema from './types/gateway-schema.json';
+import * as apiDefsSchema from './types/api-schema.json';
+import * as sourceSchema from './types/mesh-source-schema.json';
 
 export { GatewayConfig, ApiConfig, SourceConfig, ApiDef };
 
@@ -17,23 +20,20 @@ export interface Config {
 }
 
 let config: Config;
-const logger = prefixLogger('config');
+// const logger = prefixLogger('config');
 
 // JSON-Schema definitions of configuration files
-const gatewaySchema = require('./types/gateway-schema.json');
-const apiDefsSchema = require('./types/api-schema.json');
-const sourceSchema = require('./types/mesh-source-schema.json');
 
 export async function loadGatewayConfig(): Promise<GatewayConfig | null> {
-  logger.info('Loading main configuration file.');
+  // logger.info('Loading main configuration file.');
   // find & parse config
   const explorer = cosmiconfig('gateway', {
     searchPlaces: ['config/gateway.json', 'config/gateway.yaml'],
   });
 
-  const results = await explorer.search();
+  const results = await explorer.search(process.cwd());
   if (results === null) {
-    logger.error('gateway.json|yaml cannot be found.');
+    // logger.error('gateway.json|yaml cannot be found.');
     return null;
   }
 
@@ -43,7 +43,7 @@ export async function loadGatewayConfig(): Promise<GatewayConfig | null> {
     return null;
   }
 
-  config.gateway = newConfig;
+  config = config === undefined ? { gateway: newConfig, apiDefs: [] } : { gateway: newConfig, apiDefs: config.apiDefs };
 
   return config.gateway;
 }
@@ -81,12 +81,12 @@ export function validateGatewayConfig(config: any): config is GatewayConfig {
   const ajv = new Ajv();
   const validate = ajv.compile(gatewaySchema);
   if (!validate(config)) {
-    logger.error('GraphQL Portal configuration is not valid:');
-    logger.error(
-      ajv.errorsText(validate.errors, {
-        dataVar: 'config',
-      })
-    );
+    // logger.error('GraphQL Portal configuration is not valid:');
+    // logger.error(
+    //   ajv.errorsText(validate.errors, {
+    //     dataVar: 'config',
+    //   })
+    // );
     return false;
   }
 
@@ -97,12 +97,12 @@ export function validateSourceConfig(source: any): source is SourceConfig {
   const ajv = new Ajv();
   const validate = ajv.compile(sourceSchema);
   if (!validate(source)) {
-    logger.error('Source configuration is not valid:');
-    logger.error(
-      ajv.errorsText(validate.errors, {
-        dataVar: 'source',
-      })
-    );
+    // logger.error('Source configuration is not valid:');
+    // logger.error(
+    //   ajv.errorsText(validate.errors, {
+    //     dataVar: 'source',
+    //   })
+    // );
     return false;
   }
 
@@ -122,12 +122,12 @@ export function validateApiConfig(api: any): api is ApiConfig {
   const ajv = new Ajv();
   const validate = ajv.compile(apiDefsSchema);
   if (!validate(api)) {
-    logger.error('Source configuration is not valid:');
-    logger.error(
-      ajv.errorsText(validate.errors, {
-        dataVar: 'api',
-      })
-    );
+    // logger.error('Source configuration is not valid:');
+    // logger.error(
+    //   ajv.errorsText(validate.errors, {
+    //     dataVar: 'api',
+    //   })
+    // );
     return false;
   }
 
