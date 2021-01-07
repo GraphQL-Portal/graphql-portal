defe4368269da23c60b754f899d9da0ea388a578
Arthur Murauskas <arthur.murauskas@gmail.com>
2021-01-07 16:59:40 +0100

    Add hostname to ping request

--- packages/gateway/src/redis/ping.ts
+++ packages/gateway/src/redis/ping.ts
@@ -2,9 +2,10 @@ import { config } from '@graphql-portal/config';
 import { Channel } from '@graphql-portal/types';
 import { Redis } from 'ioredis';
 import { nodeId } from '../server';
+import * as os from 'os';
 
 export async function ping(redis: Redis): Promise<void> {
   setInterval(() => {
-    redis.publish(Channel.ping, JSON.stringify({ nodeId, configTimestamp: config.timestamp }));
+    redis.publish(Channel.ping, JSON.stringify({ nodeId, configTimestamp: config.timestamp, hostname: os.hostname() }));
   }, 5000);
 }
