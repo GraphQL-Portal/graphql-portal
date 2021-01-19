73bcf51f3441a2e9d032b5edb2e009d82b1ccbbb
Anton Cherednikov <a.cherednikov@code.store>
2021-01-19 11:57:14 +0200

    token;

--- packages/datasource/src/index.ts
+++ packages/datasource/src/index.ts
@@ -3,6 +3,8 @@ const GraphQLHandler = require('@graphql-mesh/graphql');
 
 export default class ContentfulHandler extends GraphQLHandler {
   constructor({ name, config, cache }: GetMeshSourceOptions<YamlConfig.GraphQLHandler>) {
+    // @ts-ignore
+    config.schemaHeaders = { authorization: `Bearer ${config.token}`};
     super({ name, config, cache});
   }
 }
