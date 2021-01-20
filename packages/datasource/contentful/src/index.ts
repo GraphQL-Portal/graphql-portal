a9a3da471555b07b7696d2183a700d76299feb57
Anton Cherednikov <a.cherednikov@code.store>
2021-01-20 11:41:25 +0200

    default endpoint;

--- packages/datasource/contentful/src/index.ts
+++ packages/datasource/contentful/src/index.ts
@@ -1,8 +1,12 @@
 import { GetMeshSourceOptions, YamlConfig } from '@graphql-mesh/types';
 const GraphQLHandler = require('@graphql-mesh/graphql');
 
+const DEFAULT_ENDPOINT: string = "https://graphql.contentful.com/";
+
 export default class ContentfulHandler extends GraphQLHandler {
   constructor({ name, config, cache }: GetMeshSourceOptions<YamlConfig.GraphQLHandler>) {
+    if (!config.endpoint) config.endpoint = DEFAULT_ENDPOINT;
+
     // @ts-ignore
     config.schemaHeaders = { authorization: `Bearer ${config.token}`};
     super({ name, config, cache});
