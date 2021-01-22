2f47eff45f6468d71be29448f8503aede4462b9a
Anton Cherednikov <a.cherednikov@code.store>
2021-01-22 11:12:46 +0200

    added definitions;

---
+++ packages/datasource/index.ts
@@ -0,0 +1,4 @@
+import contentfulDefinitiond from './contentful/src/definitions.json';
+import slackDefinitions from './slack/src/definitions.json';
+
+export default { ...contentfulDefinitiond, ...slackDefinitions };
