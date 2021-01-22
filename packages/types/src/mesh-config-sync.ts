f0f7b818a372730107be51b0edc5558342540e80
Anton Cherednikov <a.cherednikov@code.store>
2021-01-22 11:13:03 +0200

    added opportunity to update definitions;

--- packages/types/src/mesh-config-sync.ts
+++ packages/types/src/mesh-config-sync.ts
@@ -1,11 +1,15 @@
-import meshConfig from '@graphql-mesh/types/config-schema.json';
+import { definitions, properties } from '@graphql-mesh/types/config-schema.json';
 import sourceConfig from './mesh-source-schema.json';
 import apiDefConfig from './api-def-schema.json';
+import sources from '@graphql-portal/datasource';
 import fs from 'fs';
 
-sourceConfig.definitions = meshConfig.definitions as any;
+const dataSourceProperties = Object.keys(sources).map((source: string) => ({ [source]: { "$ref": `#/definitions/${source}` } }));
+definitions.Handler.properties = Object.assign({}, definitions.Handler.properties, ...dataSourceProperties);
+sourceConfig.definitions = { ...definitions, ...sources }  as any;
+
 fs.writeFileSync(`${__dirname}/mesh-source-schema.json`, JSON.stringify(sourceConfig));
 
-apiDefConfig.definitions = meshConfig.definitions as any;
-apiDefConfig.properties.mesh.properties = meshConfig.properties as any;
+apiDefConfig.definitions = definitions as any;
+apiDefConfig.properties.mesh.properties = properties as any;
 fs.writeFileSync(`${__dirname}/api-def-schema.json`, JSON.stringify(apiDefConfig));
