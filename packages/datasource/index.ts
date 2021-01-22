33e427d98143b1281f6879f8d57191d4a21ad897
Anton Cherednikov <a.cherednikov@code.store>
2021-01-22 14:34:17 +0200

    typo;

--- packages/datasource/index.ts
+++ packages/datasource/index.ts
@@ -1,4 +1,4 @@
-import contentfulDefinitiond from './contentful/src/definitions.json';
+import contentfulDefinition from './contentful/src/definitions.json';
 import slackDefinitions from './slack/src/definitions.json';
 
-export default { ...contentfulDefinitiond, ...slackDefinitions };
+export default { ...contentfulDefinition, ...slackDefinitions };
--- packages/datasource/slack/src/index.ts
+++ packages/datasource/slack/src/index.ts
@@ -1,7 +1,7 @@
 import { GetMeshSourceOptions, YamlConfig } from '@graphql-mesh/types';
 const OpenAPIHandler = require('@graphql-mesh/openapi');
 
-const OPENAPI_SPECIFICATION: string = "https://api.slack.com/specs/openapi/v2/slack_web.json";
+const OPENAPI_SPECIFICATION: string = 'https://api.slack.com/specs/openapi/v2/slack_web.json';
 
 export default class SlackHandler extends OpenAPIHandler {
   constructor({ name, config, cache }: GetMeshSourceOptions<YamlConfig.OpenapiHandler>) {
