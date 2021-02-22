856b3cbf0f775dea5f8bee9a423b6d1b893288ea
Arthur Murauskas <arthur.murauskas@gmail.com>
2021-02-22 18:47:08 +0100

    Fix ajv strict mode validation; fix eslint config

--- packages/types/src/validation.ts
+++ packages/types/src/validation.ts
@@ -2,7 +2,7 @@ import Ajv from 'ajv';
 import { sourceSchema, apiDefSchema } from '.';
 
 export function validateSourceConfig(source: any): string | void {
-  const ajv = new Ajv();
+  const ajv = new Ajv({ strict: false });
   const validate = ajv.compile(sourceSchema);
   if (!validate(source)) {
     return ajv.errorsText(validate.errors, {
@@ -12,7 +12,7 @@ export function validateSourceConfig(source: any): string | void {
 }
 
 export function validateApiDefConfig(apiDef: any): string | void {
-  const ajv = new Ajv();
+  const ajv = new Ajv({ strict: false });
   const validate = ajv.compile(apiDefSchema);
   if (!validate(apiDef)) {
     return ajv.errorsText(validate.errors, {
