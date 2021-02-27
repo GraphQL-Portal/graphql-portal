76cb077c0aabd3647d47797819401fe43cfefb01
Arthur Murauskas <arthur.murauskas@gmail.com>
2021-02-27 23:20:27 +0100

    Add tests

---
+++ packages/logger/src/test/logger.spec.ts
@@ -0,0 +1,46 @@
+import * as logger from '../index';
+import { GatewayConfig } from '@graphql-portal/types';
+
+describe('logger', () => {
+  describe('configureLogger', () => {
+    const config: GatewayConfig = {
+      hostname: 'localhost',
+      listen_port: 8080,
+      log_level: 'debug',
+      log_format: 'text',
+      use_dashboard_configs: true,
+      dashboard_config: {
+        connection_string: 'http://localhost:80',
+        secret: '123',
+      },
+      enable_control_api: false,
+      redis_connection_string: 'redis://localhost:6379',
+    };
+
+    it('should always enable console logging', () => {
+      const loggerClearSpy = jest.spyOn(logger.logger, 'clear');
+      const loggerAddSpy = jest.spyOn(logger.logger, 'add');
+
+      logger.configureLogger(config);
+      expect(loggerClearSpy).toHaveBeenCalledTimes(1);
+      expect(loggerAddSpy).toHaveBeenCalledTimes(1);
+
+      jest.clearAllMocks();
+    });
+
+    it('should add Datadog transport if it is enabled in config', () => {
+      config.datadog_logging = {
+        enabled: true,
+        apiKey: '123',
+      };
+      const loggerClearSpy = jest.spyOn(logger.logger, 'clear');
+      const loggerAddSpy = jest.spyOn(logger.logger, 'add');
+
+      logger.configureLogger(config);
+      expect(loggerClearSpy).toHaveBeenCalledTimes(1);
+      expect(loggerAddSpy).toHaveBeenCalledTimes(2);
+
+      jest.clearAllMocks();
+    });
+  });
+});
