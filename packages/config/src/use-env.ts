c2631e5873d1a64f21b5e1060e45724971baf459
Max <infant69ann@gmail.com>
2021-03-02 20:27:53 +0200

    use-env tests

--- packages/config/src/use-env.ts
+++ packages/config/src/use-env.ts
@@ -1,17 +1,18 @@
+import { apiDefSchema } from '@graphql-portal/types';
 import { deepSearch } from './utils';
 
 const envVarRegExp = /^@@/;
 
-const isArrayProperty = (key: string, schema: { [key: string]: any }): boolean => {
+const isStringArrayProperty = (key: string, schema: typeof apiDefSchema): boolean => {
   return deepSearch(schema, key, (k, v) => v?.type === 'array' && v?.items?.type === 'string');
 };
 
-export default function useEnv(config: { [key: string]: any }, schema?: { [key: string]: any }): void {
-  Object.keys(config).forEach((key) => {
+export default function useEnv(config: { [key: string]: any }, schema?: typeof apiDefSchema): void {
+  Object.keys(config).forEach((key: string) => {
     const value = config[key];
     if (typeof value === 'string' && envVarRegExp.test(value)) {
       const envValue = process.env[value.replace(envVarRegExp, '')] || '';
-      if (schema && isArrayProperty(key, schema)) {
+      if (schema && isStringArrayProperty(key, schema)) {
         config[key] = envValue.split(',');
       } else {
         config[key] = envValue;
--- packages/config/src/utils.ts
+++ packages/config/src/utils.ts
@@ -1,7 +1,28 @@
+/**
+ * Using for matching extra conditions
+ *
+ * @callback predicateCallback
+ * @param {string} key - Target key
+ * @param {*} value - Target value
+ * @return {boolean} - Boolean, true if key and value it's matches conditions
+ */
+
+/**
+ * Returns and object property according to key and predicate function.
+ *
+ * @param {string} key - Targret key
+ * @param {Object} object - Target object
+ * @param {predicateCallback} predicate - The callback that contains any extra conditions.
+ * @return {*} - Requested value
+ *
+ * @example
+ *
+ *     deepSearch({somekey: 'somevalue'}, 'somekey', (k, v) => (v === 'somevalue'))
+ */
 export const deepSearch = (
   object: { [key: string]: any },
   key: string,
-  predicate: (k: string, v: any) => boolean
+  predicate: (key: string, value: any) => boolean
 ): any => {
   if (object[key] && predicate(key, object[key])) return object[key];
 
