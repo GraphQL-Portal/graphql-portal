121dbc42028b6416195a6c32eb71acea8e431e4c
germankrause <germanswan322@gmail.com>
2021-03-07 12:59:08 +0200

    zero config

--- packages/config/src/gateway.config.ts
+++ packages/config/src/gateway.config.ts
@@ -29,12 +29,33 @@ export async function loadConfig(): Promise<GatewayConfig | null> {
     searchPlaces: ['config/gateway.json', 'config/gateway.yaml'],
   });
   const results = await explorer.search();
-  if (results === null) {
-    logger.error('gateway.json|yaml cannot be found.');
-    return null;
+  let config: GatewayConfig;
+  if (results) {
+    config = results.config;
+  } else {
+    logger.warn('config/gateway.json|yaml cannot be found, default config will be used');
+    config = {
+      hostname: 'localhost',
+      listen_port: 8080,
+      apis_path: './config/apis',
+      sources_path: './config/sources',
+      use_dashboard_configs: false,
+      enable_control_api: true,
+      enable_metrics_recording: true,
+      log_format: 'text',
+      log_level: 'info',
+      redis: {
+        connection_string: 'redis://localhost:6379',
+      },
+      control_api_config: {
+        endpoint: '/control_api',
+      },
+      cors: {
+        enabled: false,
+      },
+    };
   }
 
-  const config = results.config;
   if (!validateConfig(config)) {
     return null;
   }
